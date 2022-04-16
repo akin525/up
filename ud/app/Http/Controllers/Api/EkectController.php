acb73cd7128f4e9c82ab98381ef7838c0be7db49
IT <akinlabisamon15@gmail.com>
2022-04-16 20:34:54 +0000

    15/04/2022

--- ud/app/Http/Controllers/Api/EkectController.php
+++ ud/app/Http/Controllers/Api/EkectController.php
@@ -163,7 +163,7 @@ class EkectController
                 $response = curl_exec($curl);
 
                 curl_close($curl);
-                return $response;
+//                return $response;
 
                 $data = json_decode($response, true);
                 $success = $data["success"];
