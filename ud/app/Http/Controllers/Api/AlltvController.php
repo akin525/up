69f983b48159ed361d94b6f5bea0ef18051e56c4
IT <akinlabisamon15@gmail.com>
2022-04-16 11:03:28 +0000

    15/04/2022

--- ud/app/Http/Controllers/Api/AlltvController.php
+++ ud/app/Http/Controllers/Api/AlltvController.php
@@ -171,7 +171,7 @@ class AlltvController
                     $curl = curl_init();
 
                     curl_setopt_array($curl, array(
-                        CURLOPT_URL => 'https://test.mcd.5starcompany.com.ng/api/reseller/pay',
+                        CURLOPT_URL => $resellerURL.'pay',
                         CURLOPT_RETURNTRANSFER => true,
                         CURLOPT_ENCODING => '',
                         CURLOPT_MAXREDIRS => 10,
@@ -181,7 +181,7 @@ class AlltvController
                         CURLOPT_CUSTOMREQUEST => 'POST',
                         CURLOPT_POSTFIELDS => array('service' => 'tv', 'coded' => $tv->cat_id, 'phone' => $request->number),
                         CURLOPT_HTTPHEADER => array(
-                            'Authorization: MCDKEY_903sfjfi0ad833mk8537dhc03kbs120r0h9a'
+                            'Authorization: mcd_key_tGSkWHl5fJZsJev5FRyB5hT1HutlCa'
                         )
                     ));
 
