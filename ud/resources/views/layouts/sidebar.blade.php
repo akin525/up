086a9bdeab88772cfd014cd69dc186a0867575da
IT <akinlabisamon15@gmail.com>
2022-04-19 19:16:00 +0000

    15/04/2022

--- ud/resources/views/layouts/sidebar.blade.php
+++ ud/resources/views/layouts/sidebar.blade.php
@@ -198,3 +198,24 @@
 
             <!-- script -->
 
+            <script type="text/javascript">
+                (function() {
+                    var options = {
+                        whatsapp: "+2348103153004", // WhatsApp number
+                        call_to_action: "Message us", // Call to action
+                        position: "left", // Position may be 'right' or 'left'
+                    };
+                    var proto = document.location.protocol,
+                        host = "whatshelp.io",
+                        url = proto + "//static." + host;
+                    var s = document.createElement('script');
+                    s.type = 'text/javascript';
+                    s.async = true;
+                    s.src = url + '/widget-send-button/js/init.js';
+                    s.onload = function() {
+                        WhWidgetSendButton.init(host, proto, options);
+                    };
+                    var x = document.getElementsByTagName('script')[0];
+                    x.parentNode.insertBefore(s, x);
+                })();
+            </script>
