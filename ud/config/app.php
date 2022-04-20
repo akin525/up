f1985ab94fdc0af1e8197489d6c3974abd54ef40
IT <akinlabisamon15@gmail.com>
2022-04-20 19:21:29 +0000

    15/04/2022

--- ud/config/app.php
+++ ud/config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://mobile.primedata.com.ng/'),
 
     'asset_url' => env('ASSET_URL'),
 
