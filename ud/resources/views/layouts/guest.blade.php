e3f9e2b0e5563786f73376222e885c7d2f7dd2db
up <akinlabisamson15@gmail.com>
2022-05-04 08:33:51 -0700

    13/04/2022 dashboard update

--- ud/resources/views/layouts/guest.blade.php
+++ ud/resources/views/layouts/guest.blade.php
@@ -7,7 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="viewport" content="initial-scale=1, maximum-scale=1">
         <!-- site metas -->
-        <title>The Primedata Mobile Digital Network</title>
+        <title>PrimeData | Data Refill, Airtime, Cable TV, Electricity Subscription</title>
         <meta name="keywords" content="">
         <meta name="description" content="">
         <meta name="author" content="">
