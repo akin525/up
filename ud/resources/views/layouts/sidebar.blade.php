91d242343fbe84992e8d64ae9472ae1e207453fd
IT <akinlabisamon15@gmail.com>
2022-06-16 23:31:15 +0100

    08/06/2022

--- ud/resources/views/layouts/sidebar.blade.php
+++ ud/resources/views/layouts/sidebar.blade.php
@@ -183,7 +183,7 @@
                         <div class="card card-body align-content-center">
                             <a href="https://play.google.com/store/apps/details?id=com.wPrimeData_15605638" class="font-weight-bold text-center">
                                 <b>Download  our Mobile App</b>
-                                <img width="200" src="{{asset('images/dd.png')}}}" alt="">
+                                <img width="200" src="{{asset('images/dd.png')}}" alt="">
                             </a>
                         </div>
                     </div>
