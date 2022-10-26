34d73cc750e5fd0dfda720dda1b643fd9cc0afe6
HP <akinlabisamson15@gmail.com>
2022-10-26 22:42:18 +0100

    api redo

--- ud/routes/api.php
+++ ud/routes/api.php
@@ -26,7 +26,7 @@ Route::post('honor', [VertualController::class, 'honor'])->name('honor');
 
 Route::group(['middleware'=> 'apikey'], function () {
     Route::get('dashboard', [ResellerdetailsController::class, 'details']);
-    Route::get('airtime', [AirController::class, 'airtime']);
+    Route::post('airtime', [AirController::class, 'airtime']);
     Route::get('buydata', [BuyController::class, 'buydata']);
     Route::get('pre', [BuyController::class, 'pre']);
     Route::post('bill', [BillController::class, 'bill']);
